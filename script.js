78211d66b3ad6158cd2a476e0294760957631c21
Sardar <sardarkhurram121@gmail.com>
2024-08-28 18:29:46 +0500

    1. Commit to place/push all the files in git

---
+++ script.js
@@ -0,0 +1,36 @@
+#client-section{
+    height: 124;
+    position: relative;
+}
+
+#clients {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.client-item{
+padding: 34px;
+}
+
+#clients img{
+    height: 124;
+}
+<Section id="client-section">
+        
+        <h1 class="h-primary center">Our Clients</h1>
+        <div id="clients">
+            <div class="client-item">
+                <img src="img7.jpg" alt="Our Clients">
+            </div>
+            <!--div class="client-item">
+                <img src="img8.jpg" alt="Our Clients">
+            </div>
+            <div class="client-item">
+                <img src="img9.jpg" alt="Our Clients">
+            </div>
+            <div class="client-item">
+                <img src="img10.jpg" alt="Our Clients">
+            </div-->
+        </div>
+    </Section>
